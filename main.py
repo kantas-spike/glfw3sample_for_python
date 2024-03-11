f6a72bb3e812880e167b82d5c1e06f5f6b8a7038
kanta <101532773+kantas-spike@users.noreply.github.com>
2024-03-12 06:59:43 +0900

    sample: step02

--- main.py
+++ main.py
@@ -18,6 +18,42 @@ def setup_logger(name):
 logger = setup_logger("main.py")
 
 
+# プログラムオブジェクトを作成する
+#   vsrc: バーテックスシェーダのソースプログラムの文字列
+#   fsrc: フラグメントシェーダのソースプログラムの文字列
+def create_program(vsrc, fsrc):
+    # 空のプログラムオブジェクトを作成する
+    program = gl.glCreateProgram()
+
+    if vsrc:
+        # バーテックスシェーダのシェーダオブジェクトを作成する
+        vobj = gl.glCreateShader(gl.GL_VERTEX_SHADER)
+        gl.glShaderSource(vobj, vsrc)
+        gl.glCompileShader(vobj)
+
+        # バーテックスシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
+        gl.glAttachShader(program, vobj)
+        gl.glDeleteShader(vobj)
+
+    if fsrc:
+        # フラグメントシェーダのシェーダオブジェクトを作成する
+        fobj = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
+        gl.glShaderSource(fobj, fsrc)
+        gl.glCompileShader(fobj)
+
+        # フラグメントシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
+        gl.glAttachShader(program, fobj)
+        gl.glDeleteShader(fobj)
+
+    # プログラムオブジェクトをリンクする
+    gl.glBindAttribLocation(program, 0, "position")
+    gl.glBindFragDataLocation(program, 0, "fragment")
+    gl.glLinkProgram(program)
+
+    # 作成したプログラムオブジェクトを返す
+    return program
+
+
 def main():
     # GLFW を初期化する
     if not glfw.init():
@@ -50,10 +86,35 @@ def main():
     # 背景色を指定する
     gl.glClearColor(1.0, 1.0, 1.0, 0.0)
 
+    # バーテックスシェーダのソースプログラム
+    vsrc = """
+#version 150 core
+in vec4 position;
+void main(void)
+{
+    gl_Position = position;
+}
+"""
+
+    fsrc = """
+#version 150 core
+out vec4 fragment;
+void main(void)
+{
+    fragment = vec4(1.0, 0.0, 0.0, 1.0);
+}
+"""
+
+    # プログラムオブジェクトを作成する
+    program = create_program(vsrc, fsrc)
+
     # ウィンドウが開いている間繰り返す
     while glfw.window_should_close(window) == gl.GL_FALSE:
         # ウィンドウを消去する
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+        # シェーダプログラムの使用開始
+        gl.glUseProgram(program)
 
         #
         # ここで描画処理を行う
